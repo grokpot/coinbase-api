306efeb285bcf4d591b812a902ff40647452f15a
Ryan Prater <ryanjprater@gmail.com>
2021-12-01 23:43:15 +0100

    Added dotenv

--- runner.py
+++ runner.py
@@ -1,20 +1,20 @@
-# Requires python-requests. Install with pip:
-#
-#   pip install requests
-
-
+import os
 import json, hmac, hashlib, time, requests
 from requests.auth import AuthBase
 
-# Before implementation, set environmental variables with the names API_KEY and API_SECRET
-API_KEY = ''
-API_SECRET = ''
+from dotenv import load_dotenv
+
+# Env
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+API_SECRET = os.getenv("API_SECRET")
 
 # Create custom authentication for Coinbase API
 class CoinbaseWalletAuth(AuthBase):
     def __init__(self, api_key, secret_key):
         self.api_key = api_key
         self.secret_key = secret_key
+        print(self.api_key)
 
     def __call__(self, request):
         timestamp = str(int(time.time()))
@@ -36,6 +36,7 @@ class CoinbaseWalletAuth(AuthBase):
             'CB-ACCESS-SIGN': signature,
             'CB-ACCESS-TIMESTAMP': timestamp,
             'CB-ACCESS-KEY': self.api_key,
+            'CB-VERSION': '2021-12-01'
         })
 
         print(f"request.path_url: {request.path_url}")
@@ -44,85 +45,89 @@ class CoinbaseWalletAuth(AuthBase):
         print()
         return request
 
-api_url = 'https://api.coinbase.com'
-auth = CoinbaseWalletAuth(API_KEY, API_SECRET)
+def main():
+    api_url = 'https://api.coinbase.com'
+    auth = CoinbaseWalletAuth(API_KEY, API_SECRET)
 
-# Get current user
-# uri = '/v2/user'
-# r = requests.get(api_url + uri, auth=auth)
-# print(r.json())
+    # Get current user
+    # uri = '/v2/user'
+    # r = requests.get(api_url + uri, auth=auth)
+    # print(r.json())
 
-# List accounts
-print()
-accounts = {}
-uri = '/v2/accounts?limit=100'
-while True:
+    # List accounts
+    print()
+    accounts = {}
+    uri = '/v2/accounts?limit=100'
+    while True:
+        response = requests.get(api_url + uri, auth=auth).json()
+
+        for account in response['data']:
+            accounts[account['name']] = account
+        uri = response['pagination']['next_uri']
+        if not uri:
+            break
+    # print(f"Accounts dict: {json.dumps(accounts, indent=4, sort_keys=True, default=str)}")
+    # print(sorted(accounts.keys()))
+    # for x in accounts.values():
+    #     if x.get('id') and x.get('currency') == 'USD':
+    #         print(x['name'])
+
+    # USD
+    wallet_name = 'USD Wallet'
+    # BTC
+    # wallet_name = 'My Wallet'
+    # ETH
+    # wallet_name = 'ETH Wallet'
+
+    # list deposits
+    print()
+    print("DEPOSITS ----")
+    print(f"Account: {accounts[wallet_name]}")
+    uri = f"/v2/accounts/{accounts[wallet_name]['id']}/deposits"
     response = requests.get(api_url + uri, auth=auth).json()
+    # print(json.dumps(response, indent=4, sort_keys=True, default=str))
+    deposits = []
+    for deposit in response['data']:
+        deposits.append({
+            'id': deposit['id'],
+            'amount': deposit['amount']['amount'],
+            'currency': deposit['amount']['currency'],
+            'created_at': deposit['created_at'],
+        })
+    deposits = sorted(deposits, key=lambda k: k['created_at']) 
+    print(json.dumps(deposits, indent=4, sort_keys=True, default=str))
 
-    for account in response['data']:
-        accounts[account['name']] = account
-    uri = response['pagination']['next_uri']
-    if not uri:
-        break
-# print(f"Accounts dict: {json.dumps(accounts, indent=4, sort_keys=True, default=str)}")
-# print(sorted(accounts.keys()))
-# for x in accounts.values():
-#     if x.get('id') and x.get('currency') == 'USD':
-#         print(x['name'])
+    # list withdrawls
+    print()
+    print("WITHDRAWALS ----")
+    print(f"Account: {accounts[wallet_name]}")
+    uri = f"/v2/accounts/{accounts[wallet_name]['id']}/withdrawals"
+    response = requests.get(api_url + uri, auth=auth).json()
+    # print(json.dumps(response, indent=4, sort_keys=True, default=str))
+    withdrawals = []
+    for withdrawal in response['data']:
+        withdrawals.append({
+            'id': withdrawal['id'],
+            'amount': withdrawal['amount']['amount'],
+            'currency': withdrawal['amount']['currency'],
+            'created_at': withdrawal['created_at'],
+        })
+    withdrawals = sorted(withdrawals, key=lambda k: k['created_at']) 
+    print(json.dumps(withdrawals, indent=4, sort_keys=True, default=str))
 
-# USD
-wallet_name = 'USD Wallet'
-# BTC
-# wallet_name = 'My Wallet'
-# ETH
-# wallet_name = 'ETH Wallet'
+    # list transactions
+    print()
+    print("TRANSACTIONS ----")
+    print(f"Account: {accounts[wallet_name]}")
+    uri = f"/v2/accounts/{accounts[wallet_name]['id']}/transactions"
+    response = requests.get(api_url + uri, auth=auth).json()
+    # print(json.dumps(response, indent=4, sort_keys=True, default=str))
+    txs = []
+    for tx in response['data']:
+        txs.append(tx)
+    txs = filter(lambda t: t.get('type') not in ['buy', 'sell', 'pro_deposit', 'pro_withdrawal'], txs)
+    txs = sorted(txs, key=lambda k: k['created_at']) 
+    print(json.dumps(txs, indent=4, sort_keys=True, default=str))
 
-# list deposits
-print()
-print("DEPOSITS ----")
-print(f"Account: {accounts[wallet_name]}")
-uri = f"/v2/accounts/{accounts[wallet_name]['id']}/deposits"
-response = requests.get(api_url + uri, auth=auth).json()
-# print(json.dumps(response, indent=4, sort_keys=True, default=str))
-deposits = []
-for deposit in response['data']:
-    deposits.append({
-        'id': deposit['id'],
-        'amount': deposit['amount']['amount'],
-        'currency': deposit['amount']['currency'],
-        'created_at': deposit['created_at'],
-    })
-deposits = sorted(deposits, key=lambda k: k['created_at']) 
-print(json.dumps(deposits, indent=4, sort_keys=True, default=str))
-
-# list withdrawls
-print()
-print("WITHDRAWALS ----")
-print(f"Account: {accounts[wallet_name]}")
-uri = f"/v2/accounts/{accounts[wallet_name]['id']}/withdrawals"
-response = requests.get(api_url + uri, auth=auth).json()
-# print(json.dumps(response, indent=4, sort_keys=True, default=str))
-withdrawals = []
-for withdrawal in response['data']:
-    withdrawals.append({
-        'id': withdrawal['id'],
-        'amount': withdrawal['amount']['amount'],
-        'currency': withdrawal['amount']['currency'],
-        'created_at': withdrawal['created_at'],
-    })
-withdrawals = sorted(withdrawals, key=lambda k: k['created_at']) 
-print(json.dumps(withdrawals, indent=4, sort_keys=True, default=str))
-
-# list transactions
-print()
-print("TRANSACTIONS ----")
-print(f"Account: {accounts[wallet_name]}")
-uri = f"/v2/accounts/{accounts[wallet_name]['id']}/transactions"
-response = requests.get(api_url + uri, auth=auth).json()
-# print(json.dumps(response, indent=4, sort_keys=True, default=str))
-txs = []
-for tx in response['data']:
-    txs.append(tx)
-txs = filter(lambda t: t.get('type') not in ['buy', 'sell', 'pro_deposit', 'pro_withdrawal'], txs)
-txs = sorted(txs, key=lambda k: k['created_at']) 
-print(json.dumps(txs, indent=4, sort_keys=True, default=str))
+if __name__ == "__main__":
+    main()
